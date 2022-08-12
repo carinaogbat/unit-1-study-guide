2102398a434932ec0a746d306947259347f21e54
Carina Ogbat <carinawrightogbat@gmail.com>
2022-08-12 07:19:34 -0700

    Initial commit, added comment page for study guide.

---
+++ study-guide-comments.py
@@ -0,0 +1,126 @@
+# What is an algorithm? Provide some examples, both in the context in of computer science and beyond.
+
+# What is a heuristic? How do they relate to algorithms?
+
+# What is the brute force method? What are some problems with it?
+
+# What is version control? How is it different than simply saving a file?
+
+# What’s the difference between Git and Github?
+
+# Git commands to know:
+
+# git log
+
+# git init
+
+# git status
+
+# git add
+
+# git commit with -a and -m
+
+# git remote add
+
+# git remote -v
+
+# git push
+
+# What does the file .gitignore do? What is the content of the .gitignore file?
+
+# What are some files that git should generally ignore?
+
+# How can you tell whether a file is being ignored by git or not?
+
+# If you git add a secret file, is it possible to remove that file from the history of git?
+
+# Why is using the command line important?
+
+# What is the prompt in the context of the command line?
+
+# Shell commands to know:
+
+# ls
+
+# pwd
+
+# cp
+
+# mv
+
+# cd
+
+# mkdir
+
+# rm
+
+# man
+
+# any command with --help
+
+# CTRL-D and CTRL-C
+
+# What’s the difference between relative and absolute paths?
+
+# What’s the difference between parameters and arguments?
+
+# What’s the difference between return, break, and print?
+
+# What does a function return if it doesn’t have a return statement?
+
+# What is a default parameter?
+
+# What is scope? What is function scope?
+
+# What are some important Python style considerations for writing functions?
+
+# Python list methods/functions to know:
+
+# sorted()
+
+# .sort()
+
+# .append()
+
+# .extend()
+
+# How to index a list
+
+# How to slice a list
+
+# How to loop over a list
+
+# Using range(len(some_list))
+
+# Using a counter and indexing the list
+
+# Without using either of the above (just a for-loop)
+
+# What is mutability?
+
+# How is Python memory different than C memory? How are Python variables different than “classic” variables?
+
+# What is garbage collection?
+
+# What is the id function? What does the comparator is do in Python?
+
+# What are sets good for?
+
+# Set methods to know:
+
+# .add()
+
+# len()
+
+# .remove()
+
+# How to create a set
+
+# How to check if something is in a set
+
+# What are tuples good for?
+
+# How do you create a tuple?
+
+# How to index a tuple
+
